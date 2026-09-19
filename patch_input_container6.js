3d8bbe94e767e46a76ce2e11713b26a0ac7e670d
limzhilit <g00440629@atu.ie>
2026-09-19 07:12:33 +0100

    chore: clean up patch scripts

--- patch_input_container6.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-
-let content = fs.readFileSync('app/(center)/shop-owner/booths/page.tsx', 'utf-8');
-
-// Fix the last remaining inner icon (Send icon) that didn't get caught because it had no -ml-0.5
-content = content.replace('<Send className="h-3.5 w-3.5" />', '<Send className="h-4 w-4" />');
-
-fs.writeFileSync('app/(center)/shop-owner/booths/page.tsx', content);
